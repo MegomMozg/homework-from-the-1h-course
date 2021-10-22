f793f53cf532f78c358a1bbcee85be51d8b45094
Markus <polayna05@mail.ru>
2021-10-22 11:42:16 +0300

    lesson7

---
+++ Assets/scripts/UI/MenuCursor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuCursor : MonoBehaviour
+{
+    private void Cursormenu()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = false;
+    }
+    public void startLVL()
+    {
+        SceneManager.LoadScene(1);
+        Time.timeScale = 1;
+    }
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
